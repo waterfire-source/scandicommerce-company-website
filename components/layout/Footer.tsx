5896855334904176f8ccd128a8c8e6e4366e5725
waterfire-source <rleyayna@digiwr.com>
2025-12-18 15:59:37 +0100

    feat: update footer to use new logo image and improve layout structure

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-8 lg:gap-12 mb-12">
           <div className="lg:col-span-1">
-            <Logo />
+            <Logo logoPath='/images/footer-logo.png' />
           </div>
 
           <div>
